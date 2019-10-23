2109869bb62fdb0953e6cdc74b7d2a024a658737
Abdulmajed Dhelaimi <adhelaim@gmail.com>
2019-10-23 12:33:24 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello dear classmate")
+module.exports = () =>{
+
+    let x = 20
+
+    while(x >20){
+        console.log("Hello dear classmate.. thank you for being an awesome friend!")
+        x++
+    }
+}
