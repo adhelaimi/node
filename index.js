75cac0bdb0d3947af045102c2a87ceab4d9e4f01
Abdulmajed Dhelaimi <adhelaim@gmail.com>
2019-10-23 12:36:42 +0300

    test

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 module.exports = () =>{
 
-    let x = 20
-
-    while(x >20){
-        console.log("Hello dear classmate.. thank you for being an awesome friend!")
-        x++
-    }
+    console.log("Hello dear classmate.. thank you for being an awesome friend!")
 }
